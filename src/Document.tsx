7699ec1e861e49826c7304a4ec672c937ffbe27a
nora2605 <nora.ja2605@gmail.com>
2026-08-21 09:48:41 +0200

    embed meta

--- src/Document.tsx
+++ src/Document.tsx
@@ -7,6 +7,12 @@ export default function Document(props: ParentProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ed1c24" />
+        <meta name="og:title" content="Maeri" />
+        <meta name="og:type" content="website" />
+        <meta name="og:url" content="https://maeri.nojufe.de" />
+        <meta name="og:image" content="https://maeri.nojufe.de/favicon.ico" />
+        <meta name="og:description" content="The official Getriš Conlang Documentation and Reference" />
         <link rel="icon" href="/favicon.ico" />
         <title>Maeri</title>
         <HydrationScript />
